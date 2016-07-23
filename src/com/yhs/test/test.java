19977cd8ef1adff38a50a2cf79b5eab79da5a19c
Hansen06 <2696371886@qq.com>
2016-07-23 23:39:07 +0800

    第一次修改

---
+++ src/com/yhs/test/test.java
@@ -0,0 +1,8 @@
+package com.yhs.test;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println("DDDDDDDDD");
+	}
+}
---
+++ src/com/yhs/test/test1.java
@@ -0,0 +1,8 @@
+package com.yhs.test;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		System.out.println("CCCCCCCCCCC");
+	}
+}
